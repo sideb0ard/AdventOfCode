a13cb0978c0f6010d94a5d54bb4650a066e386bb
Thorsten Sideboard <thorsten.sideb0ard@gmail.com>
2022-12-20 19:06:17 -0800

    Up to day7 so far

--- day7.cpp
+++ day7.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <vector>
 
+constexpr int kTotalSizeDisk = 70000000;
+constexpr int kSpaceNeededForUpdate = 30000000;
+
 struct File {
   File(std::string name, int size) : name{name}, size{size} {}
   std::string name;
@@ -19,6 +22,7 @@ struct Dir {
   std::vector<Dir> dirs;
 
   int CalculateSize();
+  int CollectSizes(std::vector<int>& dir_sizes, int max_size);
   void AddFile(File file);
   void AddDir(Dir dir);
 };
@@ -31,6 +35,27 @@ int Dir::CalculateSize() {
   for (auto& d : dirs) {
     total_size += d.CalculateSize();
   }
+  std::cout << "DIR:" << name << " SIZE:" << total_size << std::endl;
+  return total_size;
+}
+
+int Dir::CollectSizes(std::vector<int>& dir_sizes, int max_size) {
+  int total_size = 0;
+  for (auto& f : files) {
+    total_size += f.size;
+  }
+  for (auto& d : dirs) {
+    total_size += d.CollectSizes(dir_sizes, max_size);
+  }
+
+  std::cout << "DIR:" << name << " SIZE:" << total_size << std::endl;
+  if (total_size > max_size) {
+    std::cout << "TOT SZ:" << total_size << " IS lESS! ADDING TO DIR SOZES\n";
+    dir_sizes.push_back(total_size);
+  } else {
+    std::cout << total_size << " is not < 100000!" << std::endl;
+  }
+
   return total_size;
 }
 
@@ -69,33 +94,34 @@ std::vector<std::string> GetTokens(std::string line) {
 
 void ReadCommands() {
   Dir* pwd = &root;
-  std::ifstream file{"day7testinput.txt"};
+  // std::ifstream file{"day7testinput.txt"};
+  std::ifstream file{"day7realinput.txt"};
   if (file.is_open()) {
     std::string line;
     while (std::getline(file, line)) {
-      std::cout << "LINE:" << line << std::endl;
+      // std::cout << "LINE:" << line << std::endl;
       auto tokens = GetTokens(line);
       if (tokens.size() > 1) {
         if (tokens[0] == "$") {
           if (tokens[1] == "cd") {
             if (tokens.size() == 3) {
-              std::cout << "CD TO DIR NAME:" << tokens[2] << std::endl;
+              // std::cout << "CD TO DIR NAME:" << tokens[2] << std::endl;
               Dir* newdir = ChangeDir(pwd, tokens[2]);
               if (newdir) {
-                std::cout << "CHANGE DIR SUCCEEDED!\n";
+                // std::cout << "CHANGE DIR SUCCEEDED!\n";
                 pwd = newdir;
               } else {
-                std::cout << "CHANGE DIR FAAAAAIl!\n";
+                // std::cout << "CHANGE DIR FAAAAAIl!\n";
               }
             }
           } else if (tokens[1] == "ls") {
-            std::cout << "ls COMMAND!\n";
+            // std::cout << "ls COMMAND!\n";
           }
         } else if (tokens[0] == "dir") {
-          std::cout << "DIR!: " << tokens[1] << "\n";
+          // std::cout << "DIR!: " << tokens[1] << "\n";
           pwd->AddDir({tokens[1], pwd});
         } else {
-          std::cout << "FILE!: " << tokens[1] << "\n";
+          // std::cout << "FILE!: " << tokens[1] << "\n";
           pwd->AddFile({tokens[1], std::stoi(tokens[0])});
         }
       }
@@ -103,4 +129,29 @@ void ReadCommands() {
   }
 }
 
-int main() { ReadCommands(); }
+void PrintDirSizes() {
+  int used_total = root.CalculateSize();
+  int free_space = kTotalSizeDisk - used_total;
+  int need_to_find = kSpaceNeededForUpdate - free_space;
+  std::cout << "Used Total:" << used_total << " Free Space:" << free_space
+            << " Need to find:" << need_to_find << std::endl;
+  std::vector<int> dir_sizes;
+  root.CollectSizes(dir_sizes, need_to_find);
+  std::cout << "YO, COLLECTED:";
+  int total = 0;
+  for (const auto& n : dir_sizes) {
+    std::cout << n << " ";
+    total += n;
+  }
+  std::cout << std::endl;
+
+  std::vector<int>::iterator result =
+      std::min_element(dir_sizes.begin(), dir_sizes.end());
+  int dist = std::distance(dir_sizes.begin(), result);
+  std::cout << "\nSMALLEST is:" << dir_sizes[dist] << std::endl;
+}
+
+int main() {
+  ReadCommands();
+  PrintDirSizes();
+}
